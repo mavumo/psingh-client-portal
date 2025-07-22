c293f61229673ca73a2edbe1b1753093443629eb
Gravitas Industries <107733141+mavumo@users.noreply.github.com>
2025-07-22 13:42:21 +0200

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// Replace with values from Firebase console > Project settings
+export const firebaseConfig = {
+  apiKey:        "YOUR_KEY",
+  authDomain:    "psingh-law.firebaseapp.com",
+  projectId:     "psingh-law",
+  storageBucket: "psingh-law.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "1:XXXX:web:XXXX"
+};
